522c8feaf19cefb7e3e96728ec28b956fad2aab0
Ujwal Subedi <hrirks@live.com>
2021-08-27 12:57:44 +0200

    Graph Implementation
    Basic Graph implementation where Node and Edges could be created as well as the value of the matrix position will be changed to 1 if Nodes has a Connection.

---
+++ Unity/UndirectedGraph/Assets/Scripts/GraphMono.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace GraphContent
+{
+    public class GraphMono : MonoBehaviour
+    {
+        public Graph _Graph;
+        public string Name;
+        public string rootName;
+
+        private void Start()
+        {
+            _Graph = new Graph(Name);
+            _Graph.CreateRoot(rootName);
+            
+        }
+
+        private void LateUpdate()
+        {
+            Debug.Log(_Graph.ToString());
+        }
+    }
+}
